2b1a1c17c068271cf0c04929f0fc732555622828
buaa.byl <buaa.byl@gmail.com>
2017-09-27 23:59:01 +0800

    save original counter instead of entropy.

--- create_pinyin_unigram.py
+++ create_pinyin_unigram.py
@@ -27,11 +27,46 @@ import math
 import json
 import sqlite3
 
+VALID_PAIRS = {
+    " " :  ["a", "ai", "an", "ang", "ao", "e", "ei", "en", "eng", "er", "o", "ou"],
+    "b" :  ["a", "ai", "an", "ang", "ao", "ei", "en", "eng", "i", "ian", "iao", "ie", "in", "ing", "o", "u"],
+    "p" :  ["a", "ai", "an", "ang", "ao", "ei", "en", "eng", "i", "ian", "iao", "ie", "in", "ing", "o", "ou", "u"],
+    "m" :  ["a", "ai", "an", "ang", "ao", "e", "ei", "en", "eng", "i", "ian", "iao", "ie", "in", "ing", "iu", "o", "ou", "u"],
+    "f" :  ["a", "an", "ang", "ei", "en", "eng", "iao", "o", "ou", "u"],
+    "d" :  ["a", "ai", "an", "ang", "ao", "e", "ei", "en", "eng", "i", "ia", "ian", "iao", "ie", "ing", "iu", "ong", "ou", "u", "uan", "ui", "un", "uo"],
+    "t" :  ["a", "ai", "an", "ang", "ao", "e", "ei", "eng", "i", "ian", "iao", "ie", "ing", "ong", "ou", "u", "uan", "ui", "un", "uo"],
+    "n" :  ["a", "ai", "an", "ang", "ao", "e", "ei", "en", "eng", "i", "ian", "iang", "iao", "ie", "in", "ing", "iu", "ong", "ou", "u", "uan", "un", "uo", "v", "ue"],
+    "l" :  ["a", "ai", "an", "ang", "ao", "e", "ei", "eng", "i", "ia", "ian", "iang", "iao", "ie", "in", "ing", "iu", "o", "ong", "ou", "u", "uan", "un", "uo", "v", "ue"],
+    "g" :  ["a", "ai", "an", "ang", "ao", "e", "ei", "en", "eng", "ong", "ou", "u", "ua", "uai", "uan", "uang", "ui", "un", "uo"],
+    "k" :  ["a", "ai", "an", "ang", "ao", "e", "ei", "en", "eng", "ong", "ou", "u", "ua", "uai", "uan", "uang", "ui", "un", "uo"],
+    "h" :  ["a", "ai", "an", "ang", "ao", "e", "ei", "en", "eng", "ong", "ou", "u", "ua", "uai", "uan", "uang", "ui", "un", "uo"],
+    "j" :  ["i", "ia", "ian", "iang", "iao", "ie", "in", "ing", "iong", "iu", "u", "uan", "ue", "un"],
+    "q" :  ["i", "ia", "ian", "iang", "iao", "ie", "in", "ing", "iong", "iu", "u", "uan", "ue", "un"],
+    "x" :  ["i", "ia", "ian", "iang", "iao", "ie", "in", "ing", "iong", "iu", "u", "uan", "ue", "un"],
+    "zh":  ["a", "ai", "an", "ang", "ao", "e", "ei", "en", "eng", "i", "ong", "ou", "u", "ua", "uai", "uan", "uang", "ui", "un", "uo"],
+    "ch":  ["a", "ai", "an", "ang", "ao", "e", "en", "eng", "i", "ong", "ou", "u", "ua", "uai", "uan", "uang", "ui", "un", "uo"],
+    "sh":  ["a", "ai", "an", "ang", "ao", "e", "ei", "en", "eng", "i", "ou", "u", "ua", "uai", "uan", "uang", "ui", "un", "uo"],
+    "r" :  ["an", "ang", "ao", "e", "en", "eng", "i", "ong", "ou", "u", "ua", "uan", "ui", "un", "uo"],
+    "z" :  ["a", "ai", "an", "ang", "ao", "e", "ei", "en", "eng", "i", "ong", "ou", "u", "uan", "ui", "un", "uo"],
+    "c" :  ["a", "ai", "an", "ang", "ao", "e", "ei", "en", "eng", "i", "ong", "ou", "u", "uan", "ui", "un", "uo"],
+    "s" :  ["a", "ai", "an", "ang", "ao", "e", "en", "eng", "i", "ong", "ou", "u", "uan", "ui", "un", "uo"],
+    "y" :  ["a", "an", "ang", "ao", "e", "i", "in", "ing", "o", "ong", "ou", "u", "uan", "ue", "un"],
+    "w" :  ["a", "ai", "an", "ang", "ei", "en", "eng", "o", "u"],
+}
+
+VALID_PAIRS_DICT = {}
+
 if __name__ == '__main__':
+    for k, vowels in VALID_PAIRS.items():
+        for vowel in vowels:
+            VALID_PAIRS_DICT["'".join([k, vowel])] = True
+
     db = sqlite3.connect('ngram_chs.db')
     cur = db.cursor()
 
     m = {}
+    valid_result = {}
+    invalid_result = {}
 
     #res = cur.execute('SELECT pinyin, freq FROM unigram LIMIT 1000')
     res = cur.execute('SELECT pinyin, freq FROM unigram')
@@ -45,35 +80,40 @@ if __name__ == '__main__':
             pinyins = row[0].split(',')
             freq    = row[1]
             for pinyin in pinyins:
+                if pinyin in VALID_PAIRS_DICT:
+                    m = valid_result
+                else:
+                    m = invalid_result
+
                 if pinyin not in m:
                     m[pinyin] = freq
                 else:
                     m[pinyin] = m[pinyin] + freq
+
     
-    total = 0
-    for pinyin, freq in m.items():
-        total = total + freq
-
-    model = {}
-    for pinyin, freq in m.items():
-        p = freq / total
-        entropy = - p * math.log(p)
-        model[pinyin] = entropy
-
     f = open('pinyin.unigram', 'w')
-    l = [(k, v) for k,v in model.items()]
+    l = [(k, v) for k,v in valid_result.items()]
     l.sort(key=lambda v:v[1], reverse=True)
-
     msg = []
     for k, v in l:
         msg.append('    %-10s: %.8f' % ('"%s"' % k, v))
-
-
     f.write('{\n')
     f.write(',\n'.join(msg))
     f.write('\n}\n')
-    #jstr = json.dumps(model, indent=4)
-    #f.write(jstr)
     f.close()
+
+    if len(invalid_result) > 0:
+        print('some pinyin pair is invalid, have a look please: "pinyin_invalid.unigram"')
+        f = open('pinyin_invalid.unigram', 'w')
+        l = [(k, v) for k,v in invalid_result.items()]
+        l.sort(key=lambda v:v[1], reverse=True)
+        msg = []
+        for k, v in l:
+            msg.append('    %-10s: %.8f' % ('"%s"' % k, v))
+
+        f.write('{\n')
+        f.write(',\n'.join(msg))
+        f.write('\n}\n')
+        f.close()
 
 
